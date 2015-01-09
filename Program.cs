db05d7867918fa1a610027c61ef1ced171b93a11
VISTALL <vistall.valeriy@gmail.com>
2015-01-09 17:42:17 +0300

    formatting. Append Message for stack trace

--- Program.cs
+++ Program.cs
@@ -1,139 +1,117 @@
-﻿using System;
-using NUnit.Core;
-#if !MONO
+﻿using NUnit.Core;
+using System;
 using System.Windows.Forms;
-#endif
-using org.mustbe.consulo.execution.testframework.thrift.runner;
 using Thrift.Protocol;
 using Thrift.Transport;
+using org.mustbe.consulo.execution.testframework.thrift.runner;
 
-namespace consulo_nunit_wrapper
-{
-	class OurEventListener : EventListener
-    {
-        private TestInterface.Client myClient;
+namespace consulo_nunit_wrapper {
+  class OurEventListener : EventListener {
+    private TestInterface.Client myClient;
 
-        private String myLastTest;
+    private String myLastTest;
 
-        public OurEventListener(TestInterface.Client client)
-        {
-            myClient = client;
-        }
-
-        public void RunFinished(Exception exception)
-        {
-            myClient.runFinished();
-        }
-
-        public void RunFinished(TestResult result)
-        {
-            myClient.runFinished();
-        }
-
-        public void RunStarted(string name, int testCount)
-        {
-            myClient.runStarted();
-        }
-
-
-        public void SuiteStarted(TestName testName)
-        {
-            myClient.suiteStarted(testName.Name, null);
-        }
-
-		#if MONO
-		public void TestFinished (TestCaseResult result)
-		{
-			if (!result.IsSuccess)
-			{
-				myClient.testFailed(result.Name, result.Message, result.StackTrace, false, null, null);
-			}
-			else
-			{
-				myClient.testFinished(result.Name, (long)result.Time);
-			}
-			myLastTest = null;
-		}
-
-		public void SuiteFinished (TestSuiteResult result)
-		{
-			myClient.suiteFinished(result.Name);
-		}
-
-		#else
-		public void SuiteFinished(TestResult result)
-		{
-			myClient.suiteFinished(result.Name);
-		}
-
-		public void TestFinished(TestResult result)
-		{
-			if (result.ResultState == ResultState.Ignored)
-			{
-				myClient.testIgnored(result.Name, result.Message, result.StackTrace);
-			}
-			else if (!result.IsSuccess)
-			{
-				myClient.testFailed(result.Name, result.Message, result.StackTrace, result.IsError, null, null);
-			}
-			else
-			{
-				myClient.testFinished(result.Name, (long)result.Time);
-			}
-			myLastTest = null;
-		}
-		#endif
-
-        public void TestOutput(TestOutput testOutput)
-        {
-            myClient.testOutput(myLastTest, testOutput.Text, testOutput.Type == TestOutputType.Out);
-        }
-
-        public void TestStarted(TestName testName)
-        {
-            myClient.testStarted(myLastTest = testName.Name, null);    
-        }
-
-        public void UnhandledException(Exception exception)
-        {
-            
-        }
+    public OurEventListener(TestInterface.Client client) {
+      myClient = client;
     }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            try
-            {
+    public void RunFinished(Exception exception) {
+      myClient.runFinished();
+    }
 
-                CoreExtensions.Host.InitializeService();
+    public void RunFinished(TestResult result) {
+      myClient.runFinished();
+    }
 
-                int port = int.Parse(args[1]);
+    public void RunStarted(string name, int testCount) {
+      myClient.runStarted();
+    }
 
-                TSocket socket = new TSocket("localhost", port);
 
-                TestInterface.Client client = new TestInterface.Client(new TBinaryProtocol(socket));
+    public void SuiteStarted(TestName testName) {
+      myClient.suiteStarted(testName.Name, null);
+    }
 
-                socket.Open();
+#if MONO
+    public void TestFinished (TestCaseResult result) {
+    if (!result.IsSuccess) {
+      myClient.testFailed(result.Name, result.Message, result.StackTrace, false, null, null);
+    }
+    else {
+      myClient.testFinished(result.Name, (long)result.Time);
+    }
+    myLastTest = null;
+  }
 
-                TestPackage testPackage = new TestPackage(args[0]);
-                RemoteTestRunner remoteTestRunner = new RemoteTestRunner();
-                remoteTestRunner.Load(testPackage);
+    public void SuiteFinished (TestSuiteResult result) {
+      myClient.suiteFinished(result.Name);
+    }
+
+#else
+    public void SuiteFinished(TestResult result) {
+    myClient.suiteFinished(result.Name);
+  }
+
+    public void TestFinished(TestResult result) {
+      if (result.ResultState == ResultState.Ignored) {
+        myClient.testIgnored(result.Name, result.Message, result.StackTrace);
+      }
+      else if (!result.IsSuccess) {
+        myClient.testFailed(result.Name, result.Message, result.StackTrace, result.IsError, null, null);
+      }
+      else {
+        myClient.testFinished(result.Name, (long)result.Time);
+      }
+      myLastTest = null;
+    }
+		#endif
+
+    public void TestOutput(TestOutput testOutput) {
+      myClient.testOutput(myLastTest, testOutput.Text, testOutput.Type == TestOutputType.Out);
+    }
+
+    public void TestStarted(TestName testName) {
+      myClient.testStarted(myLastTest = testName.Name, null);
+    }
+
+    public void UnhandledException(Exception exception) {
+
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      try {
+
+        CoreExtensions.Host.InitializeService();
+
+        int port = int.Parse(args[1]);
+
+        TSocket socket = new TSocket("localhost", port);
+
+        TestInterface.Client client = new TestInterface.Client(new TBinaryProtocol(socket));
+
+        socket.Open();
+
+        TestPackage testPackage = new TestPackage(args[0]);
+        RemoteTestRunner remoteTestRunner = new RemoteTestRunner();
+        remoteTestRunner.Load(testPackage);
 				#if MONO
-				TestResult result = remoteTestRunner.Run(new OurEventListener(client), TestFilter.Empty);
+        TestResult result = remoteTestRunner.Run(new OurEventListener(client), TestFilter.Empty);
 				#else
                 TestResult result = remoteTestRunner.Run(new OurEventListener(client), TestFilter.Empty, false, LoggingThreshold.All);
 				#endif
-            }
-            catch (Exception e)
-            {
+
+      }
+      catch(Exception e) {
 				#if !MONO
-                MessageBox.Show(e.StackTrace);
+
+  MessageBox.Show(e.Message + "\n" + e.StackTrace);
 				#else
-				System.Console.WriteLine(e.StackTrace);
+        System.Console.WriteLine(e.Message + "\n" + e.StackTrace);
 				#endif
-            }
-        }
+
+      }
     }
+  }
 }
