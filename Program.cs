cd2ea9ca7bca5345053a9b330b67feaa4f0045b4
VISTALL <vistall.valeriy@gmail.com>
2015-01-31 16:54:31 +0300

    removed dead code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using NUnit.Core;
 using System;
-#if !MONO
-using System.Windows.Forms;
-#endif
 using Thrift.Protocol;
 using Thrift.Transport;
 using org.mustbe.consulo.execution.testframework.thrift.runner;
@@ -34,25 +31,9 @@ namespace consulo_nunit_wrapper {
       myClient.suiteStarted(testName.Name, null);
     }
 
-#if MONO
-    public void TestFinished (TestCaseResult result) {
-    if (!result.IsSuccess) {
-      myClient.testFailed(result.Name, result.Message, result.StackTrace, false, null, null);
-    }
-    else {
-      myClient.testFinished(result.Name, (long)result.Time);
-    }
-    myLastTest = null;
-  }
-
-    public void SuiteFinished (TestSuiteResult result) {
+    public void SuiteFinished(TestResult result) {
       myClient.suiteFinished(result.Name);
     }
-
-#else
-    public void SuiteFinished(TestResult result) {
-    myClient.suiteFinished(result.Name);
-  }
 
     public void TestFinished(TestResult result) {
       if (result.ResultState == ResultState.Ignored) {
@@ -66,7 +47,6 @@ namespace consulo_nunit_wrapper {
       }
       myLastTest = null;
     }
-		#endif
 
     public void TestOutput(TestOutput testOutput) {
       myClient.testOutput(myLastTest, testOutput.Text, testOutput.Type == TestOutputType.Out);
@@ -98,20 +78,10 @@ namespace consulo_nunit_wrapper {
         TestPackage testPackage = new TestPackage(args[0]);
         RemoteTestRunner remoteTestRunner = new RemoteTestRunner();
         remoteTestRunner.Load(testPackage);
-				#if MONO
-        TestResult result = remoteTestRunner.Run(new OurEventListener(client), TestFilter.Empty);
-				#else
-                TestResult result = remoteTestRunner.Run(new OurEventListener(client), TestFilter.Empty, false, LoggingThreshold.All);
-				#endif
-
+        remoteTestRunner.Run(new OurEventListener(client), TestFilter.Empty, false, LoggingThreshold.All);
       }
       catch(Exception e) {
-				#if !MONO
-        MessageBox.Show(e.Message + "\n" + e.StackTrace);
-				#else
         System.Console.WriteLine(e.Message + "\n" + e.StackTrace);
-				#endif
-
       }
     }
   }
